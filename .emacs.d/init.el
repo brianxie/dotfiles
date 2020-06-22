5483378b00e64b8cf44dc3cdcf151945a68f99a6
Brian Xie <brianxie@MacBook-Pro.local>
2020-06-21 23:38:08 -0700

    Add vim and emacs dotfiles

---
+++ .emacs.d/init.el
@@ -0,0 +1,33 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (doom-modeline evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Misc
+(global-display-line-numbers-mode t)
+
+;; Evil mode
+(setq evil-toggle-key "C-c v")
+(setq evil-want-C-u-scroll t)
+(require 'evil)
+(evil-mode t)
+
+(require 'doom-modeline)
+(doom-modeline-mode 1)
---
+++ .vimrc
@@ -0,0 +1,30 @@
+set nocompatible
+
+filetype plugin indent on
+
+" Editing
+set ignorecase
+
+" Display
+set background=dark
+set hlsearch
+set incsearch
+set laststatus=2
+set number
+set ruler
+set showmatch
+set visualbell
+syntax enable
+
+" Indentation
+set autoindent
+set expandtab
+set smarttab
+set shiftwidth=2
+set softtabstop=2
+set tabstop=2
+
+" Dependencies
+colorscheme molokai
+let g:rehash256=1
+let g:molokai_original=1
