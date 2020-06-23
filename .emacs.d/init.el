7e52c62d25534c05f9d9b6e2a1208765adb8fb67
Brian Xie <brianxie@MacBook-Pro.local>
2020-06-23 14:49:38 -0700

    Add org mode to init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,7 +12,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (doom-modeline evil))))
+ '(package-selected-packages (quote (org doom-modeline evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -28,6 +28,8 @@
 (setq evil-want-C-u-scroll t)
 (require 'evil)
 (evil-mode t)
+
+(require 'org)
 
 (require 'doom-modeline)
 (doom-modeline-mode 1)
