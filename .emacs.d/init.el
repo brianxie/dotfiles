a731f1f6ff0d7e8149d9db1c4e12b7448a1af54c
Brian Xie <briancxie@gmail.com>
2020-07-18 18:17:30 -0700

    Alphabetize init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,7 +12,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (atom-one-dark-theme org doom-modeline evil))))
+ '(package-selected-packages (quote (atom-one-dark-theme doom-modeline evil org))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
