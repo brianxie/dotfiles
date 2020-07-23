e06fdb792c89b2ce8978433947a1d8390c738ecb
Brian Xie <briancxie@gmail.com>
2020-07-23 15:04:15 -0700

    Formatting and bootstrap verification

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -68,8 +68,8 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 
-;; Added by Package.el. This must come before configurations of
-;; installed packages. Don't delete this line. If you don't want it,
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
